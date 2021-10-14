1864665d0c69893a2d4df113e74cd7d6eb0c6ff8
bamjun <khy5116@naver.com>
2021-10-14 15:55:39 +0900

    Create viewset in Django

--- vdjango_django/photos/urls.py
+++ vdjango_django/photos/urls.py
@@ -5,6 +5,8 @@ from photos import views
 
 urlpatterns = [
     path('latest-products/', views.LatestProductsList.as_view()),
+    path('products/<slug:category_slug>/<slug:product_slug>/',
+         views.ProductDetail.as_view()),
 
 
 
