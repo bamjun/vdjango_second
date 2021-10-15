9e37ed912708b2c6165a66ab95ebad05709c6217
bamjun <khy5116@naver.com>
2021-10-15 14:17:12 +0900

    Sette opp Vuex / State

--- vdjango_vue/src/store/index.js
+++ vdjango_vue/src/store/index.js
@@ -2,6 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    cart: {
+      items: [],
+    },
+    isAuthenticated: false,
+    token: '',
+    isloading: false
   },
   mutations: {
   },
