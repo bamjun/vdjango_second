3626a7e9bcf1efc4627fb23e587d32f538d7ce7f
bamjun <khy5116@naver.com>
2021-10-16 15:33:35 +0900

    make it possible to view a category

--- vdjango_django/photos/urls.py
+++ vdjango_django/photos/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from django.urls.resolvers import URLPattern
+
 
 from photos import views
 
@@ -7,7 +7,5 @@ urlpatterns = [
     path('latest-products/', views.LatestProductsList.as_view()),
     path('products/<slug:category_slug>/<slug:product_slug>/',
          views.ProductDetail.as_view()),
-
-
-
+    path('products/<slug:category_slug>/', views.CategoryDetail.as_view()),
 ]
